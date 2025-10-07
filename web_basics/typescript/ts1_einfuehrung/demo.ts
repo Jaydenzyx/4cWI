6ecc560c316a4582cbe87ce58db4e631820921f0
Jaydenzyx <jayden.pribozic@student.htldornbirn.at>
2025-10-07 12:23:45 +0200

    ts1_einfuehrung_finished

---
+++ web_basics/typescript/ts1_einfuehrung/demo.ts
@@ -0,0 +1,9 @@
+let a:number = 8;
+let b:number = 6;
+
+function add (number1:number, number2:number):number {
+    return number1 + number2;
+}
+
+let result: number = add(a,b);
+console.log(result);
