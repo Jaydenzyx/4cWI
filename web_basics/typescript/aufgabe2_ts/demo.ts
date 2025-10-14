c21387262ca1e851bd6e47e7dfddf8e5c482a079
Jaydenzyx <jayden.pribozic@student.htldornbirn.at>
2025-10-14 10:31:09 +0200

    aufgabe2_ts_finished

--- web_basics/typescript/aufgabe2_ts/demo.ts
+++ web_basics/typescript/aufgabe2_ts/demo.ts
@@ -7,7 +7,7 @@ interface Person {
 
 const people: Person[] = [
     { firstname: "Max", lastname: "Mustermann", age: 30 },
-    { firstname: "Erika", lastname: "Musterfrau", age: 25 },
+    { firstname: "Erika", lastname: "Musterfrau", age: 15 },
     { firstname: "John", lastname: "Doe", age: 17 },
     { firstname: "Jane", lastname: "Doe", age:  11},
     { firstname: "Alice", lastname: "Smith", age: 18 }
@@ -17,3 +17,13 @@ const names =people.map((person) => person.firstname + " "  + person.lastname);
 console.log(names);
 const checkage = people.filter((person) => person.age >= 18);
 console.log(checkage);
+const sortetlsit = people.sort((person1 , person2) => person2.age  - person1.age);
+console.log(sortetlsit);
+const findperson = people.find((person) => person.age === 17);
+console.log(findperson);
+const averageage = people.reduce((total,person) => total +person.age/people.length,0);
+console.log(averageage);
+const gibtesmminderjährige = people.some((person) => person.age <= 11);
+console.log(gibtesmminderjährige);
+const alleüber18 = people.every((person) => person.age >= 18);
+console.log(alleüber18);
