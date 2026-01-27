04c6c3a21ac5390329c20f2e9bcf0905bda5de4e
Jaydenzyx <jayden.pribozic@student.htldornbirn.at>
2026-01-27 12:55:31 +0100

    endoflesson

--- React/4cWI_React/src/components/todoswithfilter/App.tsx
+++ React/4cWI_React/src/components/todoswithfilter/App.tsx
@@ -1,10 +1,18 @@
 import Todolist from "./Todolist";
+import {useState} from "react";
+
 
 export default function App(){
+    const [searchValue, setSearchValue] = useState("");
+
+
     return(
         <div>
             <h1>Todo</h1>
-            <Todolist />
+            <div className="mb-4">
+                <input className="border p-2" type="text" value={searchValue} onChange={(el)=>setSearchValue(el.target.value)} placeholder={"Search"}/>
+            </div>
+            <Todolist search={searchValue} />
         </div>
     );
 }
--- React/4cWI_React/src/components/todoswithfilter/Todolist.tsx
+++ React/4cWI_React/src/components/todoswithfilter/Todolist.tsx
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import TodoCard from "./TodoCard.tsx";
+import type {Todo} from "../../utils/type.ts";
 
-type Props = {};
-export default function Todolist() {
+type Props = {
+    search: string;
+};
+
+export default function Todolist({search}: Props) {
+    const [todos, setTodos] = useState<Todo[]>([]);
+    const [filteredtodos, setfilteredtodos] = useState([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(response => response.json())
+            .then(json =>
+            {setTodos(json);
+                setfilteredtodos(json);
+            })},[])
+
+    useEffect(() => {
+        console.log("searchchanged")
+        let result =todos.filter((todo:Todo) => todo.title.includes(search));
+        console.log(result)
+        }, [search]);
+
+
+    console.log("todos", todos)
+
     return (
-        <div className="grid grid-cols-5 gap-2">
-            <TodoCard title={"Putzen"} id={7} completed={false}></TodoCard>
-            <TodoCard title={"Putzen"} id={7} completed={true}></TodoCard>
-            <TodoCard title={"Putzen"} id={7} completed={true}></TodoCard>
-            <TodoCard title={"Putzen"} id={7} completed={false}></TodoCard>
-            <TodoCard title={"Putzen"} id={7} completed={true}></TodoCard>
-            <TodoCard title={"Putzen"} id={7} completed={true}></TodoCard>
-            <TodoCard title={"Putzen"} id={7} completed={false}></TodoCard>
-            <TodoCard title={"Putzen"} id={7} completed={true}></TodoCard>
-            <TodoCard title={"Putzen"} id={7} completed={true}></TodoCard>
-        </div>
+    <div className="grid grid-cols-5 gap-2">
+        {filteredtodos.map((todo: Todo) => (
+            <TodoCard key={todo.id} completed={todo.completed} title={todo.title} id={todo.id} />
+        ))}
+    </div>
     );
 };
